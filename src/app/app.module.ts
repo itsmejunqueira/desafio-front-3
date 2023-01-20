eb809e69c2be379f9cde44c38ea575edc8dec5a9
Leticia C. Junqueira <67600443+itsmejunqueira@users.noreply.github.com>
2023-01-20 10:56:54 -0300

    fix estrutura part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { AuthModule } from './auth/auth.module';
-import { DashboardModule } from './pages/dashboard/dashboard.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthModule } from './pages/auth/auth.module';
+import { AuthComponent } from './pages/auth/auth.component';
 @NgModule({
   declarations: [AppComponent, AuthComponent],
   imports: [
